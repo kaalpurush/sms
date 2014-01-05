e8434685e4c89478fef6378c79ce31e9ed17bb93
Kaal Purush <kaalbd@gmail.com>
2014-01-05 18:31:30 +0600

    worker.php

--- tests/worker.php
+++ tests/worker.php
@@ -5,11 +5,12 @@ $worker = new GearmanWorker();
 $worker->addServer();
 
 $worker->addFunction("sendSMS", function(GearmanJob $job) {
-    $workload = json_decode($job->workload(),true);
+    $workload=json_decode($job->workload(),true);
 	$provider=$workload['provider'];
 	$api_key=$workload['api_key'];
 	$api_secret=$workload['api_secret'];
-	$sms= new Kaalpurush\SMS\{$provider}($api_key,$api_secret);
+	$class="Kaalpurush\SMS\$provider";
+	$sms=new $class($api_key,$api_secret);
 	$sms->setFrom('E.HAQUE');
 	$sms->sendSMS($workload['msgdata']);
 });
